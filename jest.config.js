6621b94cf417ca075ab1bf00c8634a2f83c5748d
Peter Hozak <peter.hozak@gmail.com>
2021-01-27 12:52:08 +0100

    test: improve coverage reporting for ensureArray and print

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/.test-space/*'],
-  collectCoverageFrom: ['<rootDir>/**/*.ts', '!<rootDir>/node_modules/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageReporters: ['json'],
 };
